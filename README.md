f2afab3db5e8fab8837d428fcfaa735744581303
Iván Pérez Cóyotl <iperez.c@outlook.com>
2019-10-29 17:46:01 -0600

    first version

---
+++ README.md
@@ -0,0 +1,30 @@
+
+**Build**
+
+ - QA 
+
+ ```
+   mvn clean package -P dev
+ ```
+ 
+ ```
+   
+   `after build target\springCloudConfigServer con todos los archivos para despliegue`
+
+```
+springCloudConfigServer
+├───src
+└───target
+    └───springCloudConfigServer
+        ├───conf
+        │   ├──props
+        │   │  ├──application-example.properties
+        │   │  ...
+        │   │
+        │   └──application.properties
+        │   
+        └───spring-cloud-config-server.jar
+```
+
+
+ 
---
+++ src/main/java/com/ivoslabs/cloud/config/server/CloudConfigServerMain.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.ivoslabs.cloud.config.server;
+
+import java.io.File;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * cloud-config-server main class
+ * 
+ * @author imperezivan
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+@PropertySource("file:${app.root}/conf/application.properties")
+public class CloudConfigServerMain {
+
+    public static void main(String[] arguments) {
+	System.setProperty("app.root", new File(".").getPath());
+	SpringApplication.run(CloudConfigServerMain.class);
+    }
+
+}
